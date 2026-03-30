5e79ad5c0a4b50dd06f0d1a36d0f9761a5e83234
Adam Roehrig <arroeh01@outlook.com>
2026-03-30 16:39:34 -0400

    unit test project rename and added orchestrator tests

---
+++ Demo.Restuarants.Core.Tests/Orchestrators/RestuarantOrchestrationTests.cs
@@ -0,0 +1,225 @@
+﻿using Demo.Restuarants.Core.Orchestrations;
+using Demo.Restuarants.Infrastructure.MongoDb.Interfaces;
+
+namespace Demo.Restuarants.Core.Tests.Orchestrators;
+
+public class RestuarantOrchestrationTests
+{
+    private readonly Mock<ILogger<RestuarantOrchestration>> _mockLogger;
+    private readonly Mock<IRestuarantRepo> _mockRepo;
+    private readonly RestuarantOrchestration _orchestrator;
+
+    public RestuarantOrchestrationTests()
+    {
+        _mockLogger = new Mock<ILogger<RestuarantOrchestration>>();
+        _mockRepo = new Mock<IRestuarantRepo>();
+        _orchestrator = new(_mockLogger.Object, _mockRepo.Object);
+    }
+
+    [Test]
+    public async Task ListRestuarants_UnhandledException_ThrowsException()
+    {
+        // arrange
+        FilterQueryParametersBO mockQueryParameters = new(null, null);
+        _mockRepo.Setup(r => r.QueryRestuarants(It.IsAny<FilterQueryParametersBO>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.ListRestuarants(mockQueryParameters));
+    }
+
+    [Test]
+    public async Task ListRestuarants_QueryCompleted_ReturnsResults()
+    {
+        // arrange
+        FilterQueryParametersBO mockQueryParameters = new(null, null);
+        List<RestuarantBO> mockPaginatedData = [
+            new("", "", "", null, "", new LocationBO("", "", "", "", "")),
+            new("", "", "", null, "", new LocationBO("", "", "", "", ""))
+        ];
+        PaginationMetaData mockPaginatedMetaData = new(1, mockPaginatedData.Count, 1, mockPaginatedData.Count);
+        PaginationResponse<RestuarantBO> mockResponse = new(mockPaginatedData, mockPaginatedMetaData);
+        _mockRepo.Setup(r => r.QueryRestuarants(It.IsAny<FilterQueryParametersBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.ListRestuarants(mockQueryParameters);
+
+        // assert
+        Assert.NotNull(testResult);
+        Assert.That(testResult, Is.TypeOf<PaginationResponse<RestuarantBO>>());
+    }
+
+    [Test]
+    public async Task GetRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.GetRestuarant(mockId));
+    }
+
+    [Test]
+    public async Task GetRestuarant_RestuarantNotFound_ReturnsNull()
+    {
+        // arrange
+        string mockId = "12345";
+        RestuarantBO? mockResponse = null;
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.GetRestuarant(mockId);
+
+        // assert
+        Assert.Null(testResult);
+    }
+
+    [Test]
+    public async Task GetRestuarant_RestuarantFound_ReturnsRestuarant()
+    {
+        // arrange
+        string mockId = "12345";
+        RestuarantBO mockResponse = new(mockId, "test", "test", null, "1112223333", new LocationBO("test", "test", "KY", "test", "12345"));
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.GetRestuarant(mockId);
+
+        // assert
+        Assert.NotNull(testResult);
+        Assert.That(testResult, Is.TypeOf<RestuarantBO>());
+    }
+
+    [Test]
+    public async Task CreateRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        CreateRestuarantRequestBO mockRequest = new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345"));
+        _mockRepo.Setup(r => r.CreateRestuarant(It.IsAny<RestuarantBO>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.CreateRestuarant(mockRequest));
+    }
+
+    [Test]
+    public async Task CreateRestuarant_RestuarantFound_ReturnsRestuarant()
+    {
+        // arrange
+        CreateRestuarantRequestBO mockRequest = new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345"));
+        RestuarantBO mockResponse = new("12345", "test", "test", null, "1112223333", new LocationBO("test", "test", "KY", "test", "12345"));
+        _mockRepo.Setup(r => r.CreateRestuarant(It.IsAny<RestuarantBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.CreateRestuarant(mockRequest);
+
+        // assert
+        Assert.NotNull(testResult);
+        Assert.That(testResult, Is.TypeOf<RestuarantBO>());
+    }
+
+    [Test]
+    public async Task CreateManyRestuarants_UnhandledException_ThrowsException()
+    {
+        // arrange
+        CreateRestuarantRequestBO[] mockRequests =
+        [
+            new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345")),
+            new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345"))
+        ];
+        _mockRepo.Setup(r => r.CreateManyRestuarants(It.IsAny<RestuarantBO[]>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.CreateManyRestuarants(mockRequests));
+    }
+
+    [Test]
+    public async Task CreateManyRestuarants_QueryCompleted_ReturnsResults()
+    {
+        // arrange
+        CreateRestuarantRequestBO[] mockRequests =
+        [
+            new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345")),
+            new("test", "test", null, "1112223333", new CreateLocationRequestBO("test", "test", "KY", "test", "12345"))
+        ];
+        TransactionResult mockResponse = new(true, true, mockRequests.Length, mockRequests.Length);
+        _mockRepo.Setup(r => r.CreateManyRestuarants(It.IsAny<RestuarantBO[]>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.CreateManyRestuarants(mockRequests);
+
+        // assert
+        Assert.NotNull(testResult);
+        Assert.That(testResult, Is.TypeOf<TransactionResult>());
+    }
+
+    [Test]
+    public async Task UpdateRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        UpdateRestuarantRequestBO mockRequest = new("test", "test", null, "1112223333", new UpdateLocationRequestBO("test", "test", "KY", "test", "12345"));
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.UpdateRestuarant(mockId, mockRequest));
+    }
+
+    [Test]
+    public async Task UpdateRestuarant_RestuarantNotFound_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        UpdateRestuarantRequestBO mockRequest = new("test", "test", null, "1112223333", new UpdateLocationRequestBO("test", "test", "KY", "test", "12345"));
+        RestuarantBO? mockResponse = null;
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.UpdateRestuarant(mockId, mockRequest));
+    }
+
+    [Test]
+    public async Task UpdateRestuarant_UpdateCompleted_ReturnsResults()
+    {
+        // arrange
+        string mockId = "12345";
+        UpdateRestuarantRequestBO mockRequest = new("test", "test", null, "1112223333", new UpdateLocationRequestBO("test", "test", "KY", "test", "12345"));
+        RestuarantBO mockRestuarant = new(mockId, "test", "test", null, "1112223333", new LocationBO("test", "test", "KY", "test", "12345"));
+        TransactionResult mockResponse = new(true, true, 1, 1);
+        _mockRepo.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockRestuarant);
+        _mockRepo.Setup(r => r.UpdateRestuarant(It.IsAny<string>(), It.IsAny<UpdateRestuarantRequestBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.UpdateRestuarant(mockId, mockRequest);
+
+        // assert
+        Assert.That(testResult, Is.TypeOf<bool>());
+        Assert.True(testResult);
+    }
+
+    [Test]
+    public async Task RemoveRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        _mockRepo.Setup(r => r.RemoveRestuarant(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _orchestrator.RemoveRestuarant(mockId));
+    }
+
+    [Test]
+    public async Task RemoveRestuarant_RemoveCompleted_ReturnsResults()
+    {
+        // arrange
+        string mockId = "12345";
+        TransactionResult mockResponse = new(true, true, 1, 1);
+        _mockRepo.Setup(r => r.RemoveRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _orchestrator.RemoveRestuarant(mockId);
+
+        // assert
+        Assert.That(testResult, Is.TypeOf<bool>());
+        Assert.True(testResult);
+    }
+}
---
+++ Demo.Resuarants.API.Tests/Controllers/RestuarantControllerTests.cs
@@ -0,0 +1,253 @@
+﻿using Demo.Restuarants.API.Controllers;
+using Demo.Restuarants.API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace Demo.Resuarants.API.Tests.Controllers;
+
+public class RestuarantControllerTests
+{
+    private readonly Mock<ILogger<RestuarantController>> _mockLogger;
+    private readonly Mock<IRestuarantOrchestration> _mockOrchestration;
+    private readonly RestuarantController _controller;
+
+    public RestuarantControllerTests()
+    {
+        _mockLogger = new Mock<ILogger<RestuarantController>>();
+        _mockOrchestration = new Mock<IRestuarantOrchestration>();
+        _controller = new(_mockLogger.Object, _mockOrchestration.Object)
+        {
+            // There may be instances where http context might need to be mocked or setup
+            // This covers most of the basics that might be needed
+            ControllerContext = new ControllerContext()
+            {
+                HttpContext = new DefaultHttpContext()
+            }
+        };
+        _controller.HttpContext.Request.Headers["device-id"] = "20317";
+        _controller.HttpContext.Request.Scheme = "http";
+        _controller.HttpContext.Request.Host = new HostString("localhost");
+    }
+
+
+    [Test]
+    public async Task ListRestuarants_UnhandledException_ThrowsException()
+    {
+        // arrange
+        FilterQueryParameters mockQueryParameters = new();
+        _mockOrchestration.Setup(r => r.ListRestuarants(It.IsAny<FilterQueryParametersBO>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.ListRestuarants(mockQueryParameters));
+    }
+
+    [Test]
+    public async Task ListRestuarants_QueryCompleted_ReturnsOk()
+    {
+        // arrange
+        FilterQueryParameters mockQueryParameters = new();
+        List<RestuarantBO> mockPaginatedData = [
+            new("", "", "", null, "", new LocationBO("", "", "", "", "")),
+            new("", "", "", null, "", new LocationBO("", "", "", "", ""))
+        ];
+        PaginationMetaData mockPaginatedMetaData = new(1, mockPaginatedData.Count, 1, mockPaginatedData.Count);
+        PaginationResponse<RestuarantBO> mockResponse = new(mockPaginatedData, mockPaginatedMetaData);
+        _mockOrchestration.Setup(r => r.ListRestuarants(It.IsAny<FilterQueryParametersBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.ListRestuarants(mockQueryParameters);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Ok<PaginationResponse<RestuarantBO>>>());
+            Assert.That(((Ok<PaginationResponse<RestuarantBO>>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        });
+    }
+
+    [Test]
+    public async Task GetRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        _mockOrchestration.Setup(r => r.GetRestuarant(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.GetRestuarant(mockId));
+    }
+
+    [Test]
+    public async Task GetRestuarant_RestuarantIsNull_ReturnsNotFound()
+    {
+        // arrange
+        string mockId = "12345";
+        RestuarantBO? mockResponse = null;
+        _mockOrchestration.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.GetRestuarant(mockId);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<NotFound>());
+            Assert.That(((NotFound)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.NotFound));
+        });
+    }
+
+    [Test]
+    public async Task GetRestuarant_RestuarantFound_ReturnsOk()
+    {
+        // arrange
+        string mockId = "12345";
+        RestuarantBO mockResponse = new(mockId, "test", "test", null, "1112223333", new LocationBO("test", "test", "KY", "test", "12345"));
+        _mockOrchestration.Setup(r => r.GetRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.GetRestuarant(mockId);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Ok<RestuarantBO>>());
+            Assert.That(((Ok<RestuarantBO>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        });
+    }
+
+    [Test]
+    public async Task CreateRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        CreateRestuarantRequest mockRequest = new();
+        _mockOrchestration.Setup(r => r.CreateRestuarant(It.IsAny<CreateRestuarantRequestBO>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.CreateRestuarant(mockRequest));
+    }
+
+    [Test]
+    public async Task CreateRestuarant_CreateSuccessful_ReturnsCreated()
+    {
+        // arrange
+        CreateRestuarantRequest mockRequest = new();
+        RestuarantBO mockResponse = new("12345", "test", "test", null, "1112223333", new LocationBO("test", "test", "KY", "test", "12345"));
+        _mockOrchestration.Setup(r => r.CreateRestuarant(It.IsAny<CreateRestuarantRequestBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.CreateRestuarant(mockRequest);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Created<RestuarantBO>>());
+            Assert.That(((Created<RestuarantBO>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.Created));
+        });
+    }
+
+    [Test]
+    public async Task CreateManyRestuarants_UnhandledException_ThrowsException()
+    {
+        // arrange
+        CreateRestuarantRequest[] mockRequest = [
+            new CreateRestuarantRequest(),
+            new CreateRestuarantRequest()
+        ];
+        _mockOrchestration.Setup(r => r.CreateManyRestuarants(It.IsAny<CreateRestuarantRequestBO[]>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.CreateManyRestuarants(mockRequest));
+    }
+
+    [Test]
+    public async Task CreateManyRestuarants_OperationComplete_ReturnsOk()
+    {
+        // arrange
+        CreateRestuarantRequest[] mockRequest = [
+            new CreateRestuarantRequest(),
+            new CreateRestuarantRequest()
+        ];
+        TransactionResult mockResponse = new(true, true, mockRequest.Length, mockRequest.Length);
+        _mockOrchestration.Setup(r => r.CreateManyRestuarants(It.IsAny<CreateRestuarantRequestBO[]>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.CreateManyRestuarants(mockRequest);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Ok<TransactionResult>>());
+            Assert.That(((Ok<TransactionResult>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        });
+    }
+
+    [Test]
+    public async Task UpdateRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        UpdateRestuarantRequest mockRequest = new();
+        _mockOrchestration.Setup(r => r.UpdateRestuarant(It.IsAny<string>(), It.IsAny<UpdateRestuarantRequestBO>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.UpdateRestuarant(mockId, mockRequest));
+    }
+
+    [Test]
+    public async Task UpdateRestuarant_UpdateCompletes_ReturnsOk()
+    {
+        // arrange
+        string mockId = "12345";
+        UpdateRestuarantRequest mockRequest = new();
+        bool mockResponse = true;
+        _mockOrchestration.Setup(r => r.UpdateRestuarant(It.IsAny<string>(), It.IsAny<UpdateRestuarantRequestBO>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.UpdateRestuarant(mockId, mockRequest);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Ok<bool>>());
+            Assert.That(((Ok<bool>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        });
+    }
+
+    [Test]
+    public async Task RemoveRestuarant_UnhandledException_ThrowsException()
+    {
+        // arrange
+        string mockId = "12345";
+        _mockOrchestration.Setup(r => r.RemoveRestuarant(It.IsAny<string>())).ThrowsAsync(new Exception());
+
+        // act and assert
+        Assert.ThrowsAsync<Exception>(async () => await _controller.RemoveRestuarant(mockId));
+    }
+
+    [Test]
+    public async Task RemoveRestuarant_RemoveCompletes_ReturnsOk()
+    {
+        // arrange
+        string mockId = "12345";
+        bool mockResponse = true;
+        _mockOrchestration.Setup(r => r.RemoveRestuarant(It.IsAny<string>())).ReturnsAsync(mockResponse);
+
+        // act
+        var testResult = await _controller.RemoveRestuarant(mockId);
+
+        // assert
+        Assert.That(testResult, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(testResult, Is.TypeOf<Ok<bool>>());
+            Assert.That(((Ok<bool>)testResult).StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
+        });
+    }
+}
